78f7444acb1f638ba685c66e3e36f43e5f1e56a3
doohee <doohee006@gmail.com>
2019-07-04 00:59:20 +0900

    Initial commit

---
+++ Molraecom-wpf/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Molraecom_wpf
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
